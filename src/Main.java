ebde167fd31d58a9c12a45a2a47b66a5ce498b5f
camilabraatzz <rochacamila818@gmail.com>
2021-05-30 22:22:16 -0300

    updates

--- src/Main.java
+++ src/Main.java
@@ -3,40 +3,54 @@ import crud.FuncionarioCrud;
 import crud.ProdutoCrud;
 import crud.VendaCrud;
 import interfaces.Repository;
+import models.Cliente;
 import telas.*;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Repository repository;
         Scanner sc = new Scanner(System.in);
-        System.out.println("""
+        int count = 1;
+        while(count <= 4) {
+            System.out.println("""
                      Qual tela você deseja abrir?
                      C - Cliente
                      F - Funcionário
                      P - Produto
                      V - Venda
                     """);
-        String decisao = sc.next();
-        if (decisao.equals("C")) {
-            repository = new ClienteCrud();
-            CadastroGeral cadastroGeral =  new CadastroGeral(repository);
-            cadastroGeral.inicializar();
-        } else if (decisao.equals("F")) {
-            repository = new FuncionarioCrud();
-            CadastroGeral cadastroGeral =  new CadastroGeral(repository);
-            cadastroGeral.inicializar();
-        } else if (decisao.equals("P")) {
-            repository = new ProdutoCrud();
-            CadastroGeral cadastroGeral =  new CadastroGeral(repository);
-            cadastroGeral.inicializar();
-        } else if (decisao.equals("V")) {
-            repository = new VendaCrud();
-            CadastroGeral cadastroGeral =  new CadastroGeral(repository);
-            cadastroGeral.inicializar();
-        } else {
-            System.out.println("Você errou sua decisão, seu apedeuta!");
+            String decisao = sc.next();
+            switch (decisao) {
+                case "C" -> {
+                    repository = new ClienteCrud();
+                    CadastroGeral cadastroGeral = new CadastroGeral(repository);
+                    cadastroGeral.inicializar();
+                    break;
+                }
+                case "F" -> {
+                    repository = new FuncionarioCrud();
+                    CadastroGeral cadastroGeral = new CadastroGeral(repository);
+                    cadastroGeral.inicializar();
+                    break;
+                }
+                case "P" -> {
+                    repository = new ProdutoCrud();
+
+                    CadastroGeral cadastroGeral = new CadastroGeral(repository);
+                    cadastroGeral.inicializar();
+                    break;
+                }
+                case "V" -> {
+                    repository = new VendaCrud();
+                    CadastroGeral cadastroGeral = new CadastroGeral(repository);
+                    cadastroGeral.inicializar();
+                    break;
+                }
+                default -> System.out.println("Você errou sua decisão, seu apedeuta!");
+            }
         }
     }
 }
